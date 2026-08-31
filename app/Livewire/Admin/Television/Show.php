6aa5359377c67d99b903d7d5a8b0269ff520c4af
Dayoebe <oyetoke.ebenezer@gmail.com>
2026-08-31 17:56:41 +0100

    fix: format code for better readability in Show component

--- app/Livewire/Admin/Television/Show.php
+++ app/Livewire/Admin/Television/Show.php
@@ -15,12 +15,14 @@ class Show extends Component
 
     public function mount(Media $channel): void
     {
-        abort_unless($channel->type === MediaType::Television && ! $channel->trashed(), 404); $this->channel = $channel;
+        abort_unless($channel->type === MediaType::Television && ! $channel->trashed(), 404);
+        $this->channel = $channel;
     }
 
     public function render(): View
     {
         $this->channel->load(['country', 'administrativeArea', 'city', 'tvChannel', 'categories', 'genres', 'languages', 'artworks', 'streamSources.sourceProvider', 'streamSources.reports', 'sources.sourceProvider']);
+
         return view('livewire.admin.television.show')->layoutData(['title' => $this->channel->name]);
     }
 }
